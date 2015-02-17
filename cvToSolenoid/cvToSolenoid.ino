96dbd1c5ad9910755fca319e6a26a327fd844fe4
Václav Peloušek <info@bastl-instruments.com>
2015-02-17 10:41:35 +0100

    added motor projects

---
+++ cvToSolenoid/cvToSolenoid.ino
@@ -0,0 +1,31 @@
+
+
+#ifdef EXTERNAL_IDE
+#include <Arduino.h>
+int main(void) {
+
+  init();
+
+  setup();
+
+  while(true) {
+    loop();
+  }
+}
+
+#endif
+
+#include <EEPROM.h>
+
+void setup(){
+
+
+}
+
+void loop()
+{
+}
+
+
+
+
