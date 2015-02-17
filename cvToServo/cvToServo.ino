90d43033794417d11a80a498b9a532cb6a70b3aa
Václav Peloušek <info@bastl-instruments.com>
2015-02-17 22:34:45 +0100

    wrote basic code + tests for motors

--- cvToServo/cvToServo.ino
+++ cvToServo/cvToServo.ino
@@ -16,16 +16,56 @@ int main(void) {
 #endif
 
 #include <EEPROM.h>
+#include <Servo.h>
 
-void setup(){
+Servo srv[2];  // create servo object to control a servo
 
-
+int potpin = 0;  // analog pin used to connect the potentiometer
+int val;    // variable to read the value from the analog pin
+uint8_t analogPin[4]={1,3,0,2};
+uint16_t analogValue[4];
+#define DEBUG
+void setup()
+{
+  srv[0].attach(3);  // attaches the servo on pin 9 to the servo object
+  srv[1].attach(4);
+#ifdef DEBUG
+  Serial.begin(9600);
+#endif
 }
-
+#define CV 0
+#define POT 1
+void test(){
+	Serial.print("SA: ");
+	uint16_t angle=map(analogRead(1),0,1024,0,180);
+	srv[0].write(angle);
+	Serial.print(angle);
+	Serial.print("  SB: ");
+	angle=map(analogRead(0),0,1024,0,180);
+	srv[1].write(angle);
+	Serial.print(angle);
+	Serial.println();
+}
+void renderServo(uint8_t ch){
+	uint16_t angle;
+	angle=analogValue[ch*2+CV]-512+analogValue[ch*2+POT];
+	angle=map(angle,0,1024,0,180);
+	srv[ch].write(angle);
+}
 void loop()
 {
+#ifdef DEBUG
+	test();
+#else
+	for(int i=0;i<4;i++){
+		analogValue[i]=analogRead(analogPin[i]);
+	}
+	for(int i=0;i<2;i++){
+		renderServo(i);
+	}
+#endif
+	delay(1);
 }
-
 
 
 
--- cvToSolenoid/cvToSolenoid.ino
+++ cvToSolenoid/cvToSolenoid.ino
@@ -17,13 +17,72 @@ int main(void) {
 
 #include <EEPROM.h>
 
-void setup(){
+#define INPUT_1 B,0
+#define INPUT_2 B,1
+#define INPUT_3 B,2
+#define INPUT_4 B,3
 
+#define OUTPUT_1 D,4
+#define OUTPUT_2 D,7
+#define OUTPUT_3 D,6
+#define OUTPUT_4 D,5
+#define POT_PIN 0
+
+const uint8_t input[4]={8,9,10,11};
+const uint8_t output[4]={4,7,6,5};
+bool triggerState[4];
+#define NUMBER_OF_CHANNELS 4
+void setup(){
+	for(int i=0;i<NUMBER_OF_CHANNELS;i++){
+		pinMode(input[i],INPUT_PULLUP);
+		pinMode(output[i],OUTPUT);
+		digitalWrite(output[i],LOW);
+		triggerState[i]=false;
+	}
 
 }
+uint32_t triggerTime[4];
+bool outputState[4];
+uint32_t triggerLength;
 
 void loop()
 {
+	uint16_t newTriggerLength=analogRead(POT_PIN)>>1;
+	if(newTriggerLength!=0 && triggerLength==0){
+		for(int i=0;i<NUMBER_OF_CHANNELS;i++){
+			triggerState[i]=false;
+			outputState[i]=true;
+			//digitalWrite(output[i],LOW);
+		}
+	}
+	triggerLength=newTriggerLength;
+
+	if(triggerLength==0){
+		for(int i=0;i<NUMBER_OF_CHANNELS;i++){
+			digitalWrite(output[i],!digitalRead(input[i]));
+		}
+	}
+	else{
+		for(int i=0;i<NUMBER_OF_CHANNELS;i++){
+			bool newState=!digitalRead(input[i]);
+			if(newState && !triggerState[i]){
+				triggerTime[i]=millis();
+				digitalWrite(output[i],HIGH);
+				outputState[i]=true;
+			}
+			triggerState[i]=newState;
+		}
+
+		for(int i=0;i<NUMBER_OF_CHANNELS;i++){
+			if(outputState[i]){
+				if(millis()-triggerTime[i]>triggerLength){
+					digitalWrite(output[i],LOW);
+					outputState[i]=false;
+				}
+			}
+		}
+	}
+
 }
 
 
